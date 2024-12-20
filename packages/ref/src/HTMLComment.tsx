e27ace0e07dc54f0ee4f8b4351895873f660c694
robertpanvip <robertpanvip@163.com>
2024-12-20 18:46:21 +0800

    feat:添加 init

--- packages/ref/src/HTMLComment.tsx
+++ packages/ref/src/HTMLComment.tsx
@@ -4,48 +4,64 @@ export interface CommentProps {
     data?: string;
 }
 
-function canUseDom() {
+type ElementLike = {
+    setAttribute: () => boolean;
+    style: object;
+};
+
+const TagSymbol = `comment__`;
+
+const canUseDom = () => {
     return !!(
-        typeof window !== 'undefined' &&
+        typeof window !== "undefined" &&
         window.document &&
         window.document.createElement
     );
-}
+};
 
-const TagSymbol = `comment__`;
-const DefinedSymbol = '__defined__'
-if (canUseDom()) {
+function intercept() {
     const createElement = document.createElement;
-    if (DefinedSymbol in createElement) {
-        //react 内部是用这个创建文本节点的 由于react本身不支持创建注释节点 这里hack一下
-        document.createElement = function (
-            tagName: string,
-            options?: ElementCreationOptions
-        ) {
-            if (
-                tagName === "noscript" &&
-                options?.is &&
-                options.is.startsWith(TagSymbol)
-            ) {
-                const regex = new RegExp(`^${TagSymbol}(.*)$`);
-                const match = options?.is.match(regex);
-                if (match) {
-                    const data = match[1].trim?.();
-                    const comment = document.createComment(data);
-                    return comment as unknown as HTMLElement;
-                }
-            }
-            return createElement.call(this, tagName, options);
-        };
-        (document.createElement as unknown as { [DefinedSymbol]: boolean })[DefinedSymbol] = true
-    }
-}
 
+    const reset = () => {
+        document.createElement = createElement;
+    };
+
+    //react 内部是用这个创建文本节点的 由于react本身不支持创建注释节点 这里hack一下
+    document.createElement = function (
+        tagName: string,
+        options?: ElementCreationOptions
+    ) {
+        if (
+            tagName === "noscript" &&
+            options?.is &&
+            options.is.startsWith(TagSymbol)
+        ) {
+            const regex = new RegExp(`^${TagSymbol}(.*)$`);
+            const match = options?.is.match(regex);
+            if (match) {
+                const data = match[1].trim?.();
+                const comment = document.createComment(data) as unknown as ElementLike;
+                comment.setAttribute = () => true;
+                comment.style = {};
+                reset();
+                return comment as unknown as HTMLElement;
+            }
+        }
+        return createElement.call(this, tagName, options);
+    };
+}
 
 function CommentRender(
-    {data = ""}: CommentProps,
+    { data = "" }: CommentProps,
     ref: React.ForwardedRef<null | Comment>
 ) {
+    const mounted = React.useRef(false);
+    if (!mounted.current) {
+        if (canUseDom()) {
+            intercept();
+        }
+        mounted.current = true;
+    }
     return (
         <noscript
             ref={ref as React.ForwardedRef<null | HTMLElement>}
@@ -62,4 +78,4 @@ const HTMLComment = React.memo(
 
 HTMLComment.displayName = "Comment"; // 设置组件的 displayName，方便调试
 
-export default HTMLComment
+export default HTMLComment;
